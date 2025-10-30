ae4e81768edd7a4468087d7ae1402fe040841ed4
LuisACCosta <luisaugusto@outlook.com>
2025-10-29 21:19:50 -0300

    Pagina do menu central

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,26 @@
+// src/App.jsx
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"; // Nova dependencia do projeto: React Router
-import Login from "./pages/Login.jsx";								   // Forma de instelar: npm install react-router-dom
+import { Routes, Route, Link } from "react-router-dom";
+import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
+import MenuCentral from "./pages/MenuCentral.jsx";
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter>
-      <nav>
-        <Link to="/login">Login</Link> | <Link to="/register">Registro</Link>
+    <>
+      <nav style={{ padding: 12 }}>
+        <Link to="/login">Login</Link> |{" "}
+        <Link to="/register">Registro</Link> |{" "}
+        <Link to="/menu-central">Menu Central</Link>
       </nav>
+
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Login />} /> {/* página inicial */}
+        <Route path="/menu-central" element={<MenuCentral />} />
+        <Route path="*" element={<div>404</div>} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
-
-export default App;
