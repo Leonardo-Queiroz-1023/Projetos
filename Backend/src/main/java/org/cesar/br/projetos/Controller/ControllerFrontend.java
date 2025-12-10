78aa13eb4befea2ec3cd510e2a0aab692d8b94f4
vagnermelo2020 <vagnermontenegro2@gmail.com>
2025-12-10 09:29:30 -0300

    controller pra rodar o link

---
+++ Backend/src/main/java/org/cesar/br/projetos/Controller/ControllerFrontend.java
@@ -0,0 +1,19 @@
+package org.cesar.br.projetos.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ControllerFrontend {
+
+
+    @RequestMapping(value = "/{path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/index.html";
+    }
+
+    @RequestMapping(value = "/**/{path:[^\\.]*}")
+    public String redirectNested() {
+        return "forward:/index.html";
+    }
+}
